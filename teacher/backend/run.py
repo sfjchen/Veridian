7c7972fa2fed13aee53802974e06221890d56a63
Jonathan Chen <jchen04@stanford.edu>
2026-02-15 08:06:13 -0800

    deployment teacher

--- teacher/backend/run.py
+++ teacher/backend/run.py
@@ -1,6 +1,9 @@
+import os
+
 from app import create_app, socketio
 
 app = create_app()
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, host="0.0.0.0", port=5001)
+    port = int(os.environ.get("PORT", 5001))
+    socketio.run(app, debug=True, host="0.0.0.0", port=port)
